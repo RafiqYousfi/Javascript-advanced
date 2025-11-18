875a0216b683fd45a47886a7015cdc469d2df293
9026550@student.zadkine.nl <Ikwilkimpjes@gmail.com>
2025-11-18 09:36:27 +0100

    ;

--- 4-api/opdracht-01-fortnite-cosmetics/app.js
+++ 4-api/opdracht-01-fortnite-cosmetics/app.js
@@ -1,8 +1,22 @@
+let container = document.getElementById("container")
+
 fetch(`https://fortnite-api.com/v2/cosmetics/new`)
-.then((response) => response.json())
-.then((data) => {
- 
-for (let d of data.data.items.br)
- 
-console.log(d.name);
-});
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data.data.items.br);
+
+        for (let character of data.data.items.br) {
+            console.log(character.name);
+
+                container.innerHTML += `
+                <h2>${character.name}</h2>
+                <p>${character.description}</p>
+                <img src=${character.images.icon} alt=${character.name}>
+                `;
+
+        }
+
+
+
+
+    });
